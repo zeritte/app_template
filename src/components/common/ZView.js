c4436364867796ddac5256275fa2fd10a401f441
zeritte <zeritte@gmail.com>
2020-03-09 15:26:41 +0300

    zview update

--- src/components/common/ZView.js
+++ src/components/common/ZView.js
@@ -5,9 +5,9 @@ export const ZView = props => {
   const { children, style, flex, centerX, centerY, centerSelf, width, fullWidth } = props;
   let styles = {};
   if (flex) styles["flex"] = 1;
-  if (centerX) styles = { flex: 1, alignItems: "center", ...styles };
-  if (centerY) styles = { flex: 1, justifyContent: "center", ...styles };
-  if (centerSelf) styles = { flex: 1, alignSelf: "center", ...styles };
+  if (centerX) styles = { alignItems: "center", ...styles };
+  if (centerY) styles = { justifyContent: "center", ...styles };
+  if (centerSelf) styles = { alignSelf: "center", ...styles };
   if (fullWidth) styles["width"] = "100%";
   if (width) styles["width"] = width;
   return <View style={[styles, style]}>{children}</View>;
